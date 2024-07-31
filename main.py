a3ece7f9d2770231e496940c01c2ea7672ada6b5
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2024-07-31 09:39:08 +0300

    data_transoformation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from text_summarizer.logging import logger
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -20,6 +22,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -1,11 +1,14 @@
 from text_summarizer.constants import *
 from text_summarizer.utils.common import read_yaml, create_directories
-from text_summarizer.entity import (DataValidationConfig, DataIngestionConfig)
+from text_summarizer.entity import (
+    DataValidationConfig, DataIngestionConfig, DataTransformationConfig
+)
 
 class ConfigurationManager:
-    def __init__(self):
-        config_filepath = CONFIG_FILE_PATH
-        params_filepath = PARAMS_FILE_PATH
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
@@ -23,9 +26,12 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir
+            unzip_dir=config.unzip_dir 
         )
+
         return data_ingestion_config
+    
+
 
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
@@ -39,3 +45,17 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
